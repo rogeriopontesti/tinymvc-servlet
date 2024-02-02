0fb38a7debb0657952f309df5c27f44996991779
Rogerio Pontes <rogeriopontesti@gmail.com>
2024-02-02 14:45:02 -0300

    Resolvido problemas com invoke reflection no metodo

--- src/java/app/classes/Main.java
+++ src/java/app/classes/Main.java
@@ -7,6 +7,13 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.jsp.*;
+import java.lang.reflect.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.lang.*;
+import java.lang.reflect.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.ClassNotFoundException;
 
 public class Main extends HttpServlet {
 
@@ -66,22 +73,22 @@ public class Main extends HttpServlet {
     }
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+//        response.setContentType("text/html;charset=UTF-8");
 
         String url = this.preparaURL(request, response);
 
         String ctrl = this.preparaController(url);
         String controller = ctrl + "Controller";
         controller = controller.substring(0, 1).toUpperCase() + controller.substring(1);
+        String pacote = "app.controllers." + controller;
 
         String md = ctrl;
         String model = md + "Model";
         model = model.substring(0, 1).toUpperCase() + model.substring(1);
 
         String action = this.preparaAction(url);
-        
-        UsersController users = new UsersController();
 
+//        UsersController users = new UsersController();
         this.setCtrl(ctrl);
         this.setController(controller);
         this.setMd(md);
@@ -89,52 +96,146 @@ public class Main extends HttpServlet {
         this.setAction(action);
         this.setArgs(this.preparaArgs(url));
 
-//        try (PrintWriter out = response.getWriter()) {
+        try (PrintWriter out = response.getWriter()) {
 
-//            out.println("url: " + url);
-//            out.println("ctrl: " + ctrl);
-//            out.println("controller: " + controller);
-//            out.println("md: " + md);
-//            out.println("model: " + model);
-//            out.println("action: " + action);
-//            out.println("");
-//            out.println("");
+            out.println("url: " + url);
+            out.println("ctrl: " + ctrl);
+            out.println("controller: " + controller);
+            out.println("md: " + md);
+            out.println("model: " + model);
+            out.println("action: " + action);
+            out.println("pacote: " + pacote);
+
+            out.println("-------------------------------------------------------");
+            for (int i = 0; i < this.args.size(); i++) {
+                out.println("Parametro " + (i + 1) + ") " + this.args.get(i));
+            }
+
+            try {
+                Class<?> cls = Class.forName(pacote);
+                UsersController users = new UsersController();
+                
+                try {
+                    Class[] parameterType = new Class[3];
+                    parameterType[0] = ArrayList.class;
+                    parameterType[1] = HttpServletRequest.class;
+                    parameterType[2] = HttpServletResponse.class;
+
+                    Method mtd = cls.getMethod(action, parameterType);
+//                    Method mtd = cls.getMethod(action);
+
+                    Object[] params = new Object[3];
+                    params[0] = this.args;
+                    params[1] = request;
+                    params[2] = response;
+                    
+                    try {
+                        mtd.invoke(users, params);
+                        out.println("users ok");
+                    } catch (IllegalAccessException e) {
+                        out.println("IllegalAccessException e");
+                        out.println(e.toString());
+                        out.println(e.getMessage());
+                    } catch (IllegalArgumentException e) {
+                        out.println("IllegalArgumentException e");
+                        out.println(e.toString());
+                        out.println(e.getMessage());
+                    } catch (InvocationTargetException e) {
+                        out.println("InvocationTargetException e");
+                        out.println(e.toString());
+                        out.println(e.getMessage());
+                    }
+
+                    out.println(Arrays.toString(params));
+
+                } catch (NoSuchMethodException methodException) {
+                    out.println("NoSuchMethodException: methodException");
+                    out.println("Line: 118");
+                    out.println(methodException.toString());
+                    out.println(methodException.getMessage());
+                }
+
+//                try {
+//                    Class[] parameterType = new Class[2];
+//                    parameterType[0] = ArrayList.class;
+//                    parameterType[1] = HttpServletRequest.class;
+//                    parameterType[2] = HttpServletResponse.class;
+//                    out.println("Show 2");
+//                    Method mtd = cls.getDeclaredMethod(action, parameterType);
+//                    out.println("Show");
+//                    try {
+//                        Object[] params = new Object[3];
+//                        params[0] = this.args;
+//                        params[1] = request;
+//                        params[2] = response;
+//                        Object resultado = cls.class.invoke(controller, params);
+//                    } catch (IllegalAccessException ilegalException) {
+//                        out.println("IllegalAccessException: ilegalException");
+//                        out.println("Line: 118");
+//                        out.println(ilegalException.toString());
+//                    }
+//                } catch (NoSuchMethodException methodException) {
+//                    out.println("NoSuchMethodException: methodException");
+//                    out.println("Line: 118");
+//                    out.println(methodException.toString());
+//                }
+//                Constructor<?>[] construtores = cls.getDeclaredConstructors();
+                //todos os construtores
+//                out.println("-------------------------------------------------------");
+//                for (int i = 0; i < construtores.length; i++) {
+//                    out.println(construtores[i]);
+//                }
+                //todos os atributos da classe
+//                Field[] atributos = cls.getDeclaredFields();
 //
-//            for (int i = 0; i < this.args.size(); i++) {
-//                out.println("Parametro " + i + ") " + this.args.get(i));
-//            }
+//                out.println("-------------------------------------------------------");
+//                for (int i = 0; i < atributos.length; i++) {
+//                    out.println(atributos[i]);
+//                }
+                //todos os atributos públicos
+//                Field[] attrs = cls.getFields();
+//
+//                out.println("-------------------------------------------------------");
+//                for (int i = 0; i < attrs.length; i++) {
+//                    out.println(attrs[i]);
+//                }
+                //todos os métodos da classe
+//                Method[] metodos = cls.getDeclaredMethods();
+//
+//                out.println("-------------------------------------------------------");
+//                for (int i = 0; i < metodos.length; i++) {
+//                    out.println(metodos[i]);
+//                    Class<?>[] parametros = metodos[i].getParameterTypes();
+//                    for (int x = 0; x < parametros.length; x++) {
+//                        out.println("Parametro " + x + ": " + parametros[x]);
+//                    }
+//                    out.println("");
+//                }
+                //todos os métodos públicos
+//                Method[] mtds = cls.getMethods();
+//
+//                out.println("-------------------------------------------------------");
+//                for (int i = 0; i < mtds.length; i++) {
+//                    out.println(mtds[i]);
+//                    Class<?>[] params = mtds[i].getParameterTypes();
+//                    for (int x = 0; x < params.length; x++) {
+//                        out.println("Parametro " + x + ": " + params[x]);
+//                    }
+//                    out.println("");
+//                }
+            } catch (ClassNotFoundException exception) {
+                out.println("ClassNotFoundException: exception");
+                out.println("Package: app.classes");
+                out.println("Class Name: Main");
+                out.println("Method: processRequest");
+                out.println("Line: 118");
+                out.println(exception.toString());
+                out.println(exception.getMessage());
+            }
 
-//            out.println(linkurl.contains("/"));
+            this.args.clear();
+        }
 
-            /* TODO output your page here. You may use following sample code. */
-//            out.println("<!DOCTYPE html>");
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet TinyListeners</title>");            
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<h1>" + linkurl + "</h1>");
-//            out.println("<h1>Servlet TinyListeners at " + request.getContextPath() + "</h1>");
-//            out.println("<p>request.getScheme() " + request.getScheme() + "</p>");
-//            out.println("<p>request.getScheme() " + request.getScheme() + "</p>");
-//            out.println("<p>request.getServerName() " + request.getServerName() + "</p>");
-//            out.println("<p>request.getServerPort() " + request.getServerPort() + "</p>");
-//            out.println("<p>request.getRequestURI() " + request.getRequestURI() + "</p>");
-//            out.println("<p>request.getRequestURL() " + request.getRequestURL() + "</p>");
-//            out.println("<p>URL " + request.getScheme() + "</p>");
-//            out.println("<p>DOMAIN " + domain + "</p>");
-//            out.println("</body>");
-//            out.println("</html>");
-//        }
-
-        this.args.clear();
-        
-//        getServletContext().getRequestDispatcher("/view/users/add.jsp").forward(request, response);
-        
-        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/view/users/add.jsp");
-        dispatcher.forward(request, response);
-        
-//        getServletContext().getRequestDispatcher("/view/users/add.jsp").forward(request, response);
     }
 
     private String preparaURL(HttpServletRequest request, HttpServletResponse response) {
@@ -186,7 +287,7 @@ public class Main extends HttpServlet {
 
     private ArrayList<String> preparaArgs(String url) {
         ArrayList<String> list = new ArrayList<String>();
-                
+
         if (!url.equals("") && url.contains("/")) {
             String[] elem = url.split("/");
             for (int i = 0; i < elem.length; i++) {
@@ -195,25 +296,23 @@ public class Main extends HttpServlet {
                 }
             }
         }
-        
+
         return list;
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
--- src/java/app/controllers/UsersController.java
+++ src/java/app/controllers/UsersController.java
@@ -10,24 +10,27 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /**
  *
  * @author 28934
  */
 public class UsersController extends HttpServlet {
+    
+    
+    public String name = null;
+    private String username = null;
+    private String password = null;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    public UsersController(){
+    }
+    
+    public UsersController(String teste){
+        
+    }
+    
+    public void add(ArrayList<String> args, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -41,45 +44,6 @@ public class UsersController extends HttpServlet {
             out.println("</body>");
             out.println("</html>");
         }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    } 
+    
 }
